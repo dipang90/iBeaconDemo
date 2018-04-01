da30ccfaca0c424205b385b703e09ac4c9023d56
dipang90 <shethdipang@gmail.com>
2018-04-01 19:02:52 +0530

    Initial Commit

---
+++ iBeaconDemo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  iBeaconDemo-Bridging-Header.h
+//  iBeaconDemo
+//
+//  Created by Dipang Sheth on 01/04/18.
+//  Copyright © 2018 Dipang. All rights reserved.
+//
+
+#ifndef iBeaconDemo_Bridging_Header_h
+#define iBeaconDemo_Bridging_Header_h
+
+
+#endif /* iBeaconDemo_Bridging_Header_h */
+#import <EstimoteSDK/EstimoteSDK.h>
